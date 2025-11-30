7ec0631e12acfe0e8633e88c50d013732761ebd3
jayheid <jp.heidbrink@gmail.com>
2025-11-30 11:56:04 -0600

    adding macro to clean models last altered to parameterized value

--- models/staging/scratch.sql
+++ models/staging/scratch.sql
@@ -1,1 +1,12 @@
-{{ union_tables_by_prefix(database = "raw", schema = "dbt_learn_jinja", prefix = "orders__")}}
+select 
+    table_type
+    table_schema,
+    table_name,
+    last_altered,
+    case when table_type = 'VIEW' then 'VIEW' else 'TABLE' end as object_type,
+    'DROP ' || object_type || ' ' || '{{ database | upper }}' || '.' || table_schema || '.' || table_name || ';' as drop_statement
+from {{ database }}.information_schema.tables
+where 1=1 
+    and table_schema = upper('{{ schema }}')
+    and date(last_altered) <= date(dateadd('day', -2, current_date))
+order by 1 desc
