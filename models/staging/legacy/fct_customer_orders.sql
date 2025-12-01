a3cfa9165d477cedfc129ad40ebc17d4611f3ab8
jayheid <jp.heidbrink@gmail.com>
2025-12-01 17:33:40 -0600

    modifying sources

--- models/staging/legacy/fct_customer_orders.sql
+++ models/staging/legacy/fct_customer_orders.sql
@@ -1,47 +1,93 @@
-WITH paid_orders as (select Orders.ID as order_id,
-    Orders.USER_ID	as customer_id,
-    Orders.ORDER_DATE AS order_placed_at,
-        Orders.STATUS AS order_status,
-    p.total_amount_paid,
-    p.payment_finalized_date,
-    C.FIRST_NAME    as customer_first_name,
-        C.LAST_NAME as customer_last_name
-FROM raw.jaffle_shop.orders as Orders
-left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-        from raw.stripe.payment
-        where STATUS <> 'fail'
-        group by 1) p ON orders.ID = p.order_id
-left join raw.jaffle_shop.customers C on orders.USER_ID = C.ID ),
+-- sources
+with orders as (
+    select
+        *
+    from {{ref('stg_jaffle_shop_orders')}}      
+),
+customers as (
+    select 
+        *
+    from {{ref('stg_jaffle_shop_customers')}}
 
-customer_orders 
-as (select C.ID as customer_id
-    , min(ORDER_DATE) as first_order_date
-    , max(ORDER_DATE) as most_recent_order_date
-    , count(ORDERS.ID) AS number_of_orders
-from raw.jaffle_shop.customers C 
-left join raw.jaffle_shop.orders as Orders
-on orders.USER_ID = C.ID 
-group by 1)
+),
 
-select
-p.*,
-ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
-ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-CASE WHEN c.first_order_date = p.order_placed_at
-THEN 'new'
-ELSE 'return' END as nvsr,
-x.clv_bad as customer_lifetime_value,
-c.first_order_date as fdos
-FROM paid_orders p
-left join customer_orders as c USING (customer_id)
-LEFT OUTER JOIN 
-(
-        select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+payments as (
+    select
+        *
+    from 
+        {{ref('stg_stripe_payments')}}
+
+),
+
+-- basic staging
+
+-- intermediate
+paid_orders as (
+    select 
+        orders.order_id as order_id,
+        orders.customer_id	as customer_id,
+        orders.order_date as order_placed_at,
+        orders.status as order_status,
+        p.total_amount_paid,
+        p.payment_finalized_date,
+        c.first_name    as customer_first_name,
+        c.last_name as customer_last_name
+    from orders
+    left join (
+        select order_id as order_id, max(created_at) as payment_finalized_date, sum(amount) / 100.0 as total_amount_paid
+        from 
+            payments
+        where 
+            status <> 'fail'
+        group by 1) p on orders.order_id = p.order_id
+    left join customers c on orders.customer_id = c.customer_id ),
+
+customer_orders as (
+    select c.customer_id as customer_id
+    , min(order_date) as first_order_date
+    , max(order_date) as most_recent_order_date
+    , count(orders.order_id) as number_of_orders
+    from 
+        customers c 
+        left join orders on orders.customer_id = c.customer_id 
     group by 1
-    order by p.order_id
-) x on x.order_id = p.order_id
-ORDER BY order_id
+),
+
+
+-- final
+
+
+final as (
+select
+    p.*,
+    row_number() over (order by p.order_id) as transaction_seq,
+    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+    case when c.first_order_date = p.order_placed_at
+    then 'new'
+    else 'return' end as nvsr,
+    x.clv_bad as customer_lifetime_value,
+    c.first_order_date as fdos
+from 
+    paid_orders p
+    left join customer_orders as c using (customer_id)
+    left join 
+    (
+        select
+            p.order_id,
+            sum(t2.total_amount_paid) as clv_bad
+        from 
+            paid_orders p
+            left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+        group by 1
+        order by p.order_id
+    ) x on x.order_id = p.order_id
+    -- order by order_id
+)
+
+
+select 
+    *
+from 
+    final
+order by 
+    order_id
