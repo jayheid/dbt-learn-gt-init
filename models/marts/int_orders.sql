af7c14c2f175471fc300b8b09119d3fc9fc32867
jayheid <jp.heidbrink@gmail.com>
2025-12-04 15:55:32 -0600

    modifying final output

--- models/marts/int_orders.sql
+++ models/marts/int_orders.sql
@@ -11,6 +11,14 @@ payments as (
         {{ref('stg_stripe_payments')}}
 
 ),
+customers as (
+    select
+        customer_id,
+        first_name as customer_first_name,
+        last_name as customer_last_name
+    from 
+        {{ref('stg_jaffle_shop_customers')}}
+),
 
 -- logical
 completed_payments as (
@@ -32,8 +40,11 @@ paid_orders as (
         orders.status as order_status,
         completed_payments.total_amount_paid,
         completed_payments.payment_finalized_date,
+        customers.customer_first_name,
+        customers.customer_last_name
     from orders
     left join completed_payments on orders.order_id = completed_payments.order_id
+    left join customers on orders.customer_id = customers.customer_id
 )
 
 select 
--- models/staging/legacy/fct_customer_orders.sql
+++ models/staging/legacy/fct_customer_orders.sql
@@ -9,12 +9,18 @@ paid_orders as (
 -- final
 final as (
 select
-    paid_orders.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+    customer_id,
+    order_placed_at,
+    order_status,
+    total_amount_paid,
+    payment_finalized_date,
+    customer_first_name,
+    customer_last_name,
+    row_number() over (order by order_id) as transaction_seq,
+    row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
     case 
-        (when rank() over (partition by customer_id 
-            order by order_placed_at, order_id)) = 1
+        when rank() over (partition by customer_id 
+            order by order_placed_at, order_id) = 1
         then 'new'
         else 'return' 
     end as nvsr,
@@ -33,5 +39,3 @@ select
     *
 from 
     final
-order by 
-    order_id
